801293a59629bd8f431e215bf22d01c9a54facfc
guillermo-ggs <ggsparacino@gmail.com>
2020-10-10 14:57:09 -0400

    reupload everything again

---
+++ core/src/dev/fong/hackathongame/Level1.java
@@ -0,0 +1,11 @@
+package dev.fong.hackathongame;
+
+import com.badlogic.gdx.ScreenAdapter;
+
+public class Level1 extends ScreenAdapter {
+    MainGame game;
+
+    public Level1(MainGame game) {
+        this.game = game;
+    }
+}

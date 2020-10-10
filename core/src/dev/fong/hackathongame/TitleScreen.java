12039ef8c4bb9e3b8deb674b7077ac313bf4c2de
Olivia Beck <obeck913@knights.ucf.edu>
2020-10-10 15:07:07 -0400

    basic title screen functions added.

--- core/src/dev/fong/hackathongame/TitleScreen.java
+++ core/src/dev/fong/hackathongame/TitleScreen.java
@@ -5,13 +5,29 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+
 
 public class TitleScreen extends ScreenAdapter {
+    private static final int BTN_WIDTH = 200;
+    private static final int BTN_HEIGHT = 100;
+
     MainGame game;
+
+    Texture playBtn;
+    Texture playBtnHover;
+    Texture exitBtn;
+    Texture exitBtnHover;
 
     public TitleScreen(MainGame game) {
         this.game = game;
+
+        playBtn = new Texture("playbtn.png");
+        playBtnHover = new Texture("playbtnhover.png");
+        exitBtn = new Texture("exitBtn.png");
+        exitBtnHover = new Texture("exitBtnHover.png");
     }
+
 
     @Override
     public void show(){
@@ -28,12 +44,31 @@ public class TitleScreen extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, .25f, 0, 1);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
-        game.font.draw(game.batch, "Title Screen!", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .75f);
-        game.font.draw(game.batch, "Click the circle to win.", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .5f);
-        game.font.draw(game.batch, "Press space to play.", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .25f);
+
+        int x = (Gdx.graphics.getWidth() / 2) - (BTN_WIDTH / 2);
+        int y = Gdx.graphics.getHeight() / 2;
+        int playBtnY = y - 100;
+
+        if(Gdx.input.getX() < x + BTN_WIDTH && Gdx.input.getX() > x && Gdx.input.getY() < y + BTN_HEIGHT && Gdx.input.getY() > y){
+            game.batch.draw(exitBtnHover, x, playBtnY, BTN_WIDTH, BTN_HEIGHT);
+        }
+        else {
+            game.batch.draw(exitBtn, x, playBtnY, BTN_WIDTH, BTN_HEIGHT);
+        }
+
+        if(Gdx.input.getX() < x + BTN_WIDTH && Gdx.input.getX() > x && Gdx.input.getY() < playBtnY + BTN_HEIGHT && Gdx.input.getY() > playBtnY){
+            game.batch.draw(playBtnHover, x, y, BTN_WIDTH, BTN_HEIGHT);
+        }
+        else {
+            game.batch.draw(playBtn,x, y, BTN_WIDTH, BTN_HEIGHT);
+        }
+
+        //game.font.draw(game.batch, "GAME", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .75f);
+        //game.font.draw(game.batch, "Click the circle to win.", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .5f);
+        //game.font.draw(game.batch, "Press space to play.", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .25f);
         game.batch.end();
     }
 

a8265264757a3fc2329dbcad651463bf8168adc8
guillermo-ggs <ggsparacino@gmail.com>
2020-10-10 17:36:38 -0400

    created renderer for level1

--- core/src/dev/fong/hackathongame/Level1.java
+++ core/src/dev/fong/hackathongame/Level1.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -16,12 +19,33 @@ public class Level1 extends ScreenAdapter {
     private OrthographicCamera gamecam;
     private Viewport gamePort;
 
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
     public Level1(MainGame game) {
         atlas = new TextureAtlas("Akemi.pack");
         this.game = game;
         texture = new Texture("GohanCalvo.jpg");
         gamecam = new OrthographicCamera();
         gamePort = new FitViewport(480, 208, gamecam);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+
+        gamecam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+    }
+
+    public void handleInput(float dt) {
+        if (Gdx.input.isTouched())
+            gamecam.position.x += 100 * dt;
+    }
+
+    public void update(float dt) {
+        handleInput(dt);
+        gamecam.update();
+        renderer.setView(gamecam);
     }
 
     public TextureAtlas getAtlas(){
@@ -35,11 +59,12 @@ public class Level1 extends ScreenAdapter {
 
     @Override
     public void render(float delta){
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        update(delta);
+
+        Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.begin();
-        game.batch.draw(texture, 0, 0);
-        game.batch.end();
+
+        renderer.render();
     }
 
     @Override

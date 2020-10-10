c7a61bee9f815e90aca99bf447fd406a6cd667bb
guillermo-ggs <61523876+guillermo-ggs@users.noreply.github.com>
2020-10-10 01:52:48 -0400

    added new files

---
+++ desktop/src/dev/fong/hackathongame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dev.fong.hackathongame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dev.fong.hackathongame.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}

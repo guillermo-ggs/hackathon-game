5a4f24a9abc55a8b7955645f43b2a41478ac9f03
Olivia Beck <obeck913@knights.ucf.edu>
2020-10-11 01:58:05 -0400

    title background added and coordinate adjustments for menu buttons

--- core/src/dev/fong/hackathongame/TitleScreen.java
+++ core/src/dev/fong/hackathongame/TitleScreen.java
@@ -11,6 +11,8 @@ import com.badlogic.gdx.graphics.Texture;
 public class TitleScreen extends ScreenAdapter {
     public static final int BTN_WIDTH = 200;
     public static final int BTN_HEIGHT = 100;
+    public static final int TITLE_WIDTH = 700;
+    public static final int TITLE_HEIGHT = 200;
 
     MainGame game;
 
@@ -18,6 +20,8 @@ public class TitleScreen extends ScreenAdapter {
     Texture playBtnHover;
     Texture exitBtn;
     Texture exitBtnHover;
+    Texture gameTitle;
+    Texture background;
 
     public TitleScreen(MainGame game) {
         this.game = game;
@@ -26,6 +30,8 @@ public class TitleScreen extends ScreenAdapter {
         playBtnHover = new Texture("playbtnhover.png");
         exitBtn = new Texture("exitBtn.png");
         exitBtnHover = new Texture("exitBtnHover.png");
+        gameTitle = new Texture("gametitle.png");
+        background = new Texture("background.png");
     }
 
 
@@ -44,33 +50,36 @@ public class TitleScreen extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0.8f, 1, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         game.batch.begin();
 
         int x = (Gdx.graphics.getWidth() / 2) - (BTN_WIDTH / 2);
-        int y = Gdx.graphics.getHeight() / 2;
-        int playBtnY = y - 100;
+        int y = (Gdx.graphics.getHeight() / 2) + (BTN_HEIGHT / 2);
+        int gameTitleY = y + 100;
+
+        game.batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        game.batch.draw(gameTitle, (Gdx.graphics.getWidth() / 2) - (TITLE_WIDTH / 2), gameTitleY, TITLE_WIDTH, TITLE_HEIGHT);
 
         if(Gdx.input.getX() < x + BTN_WIDTH && Gdx.input.getX() > x && Gdx.input.getY() < y + BTN_HEIGHT && Gdx.input.getY() > y){
-            game.batch.draw(exitBtnHover, x, playBtnY, BTN_WIDTH, BTN_HEIGHT);
+            game.batch.draw(exitBtnHover, x, y - 200, BTN_WIDTH, BTN_HEIGHT);
             if(Gdx.input.isTouched()) {
                 Gdx.app.exit();
             }
         }
         else {
-            game.batch.draw(exitBtn, x, playBtnY, BTN_WIDTH, BTN_HEIGHT);
+            game.batch.draw(exitBtn, x, y - 200, BTN_WIDTH, BTN_HEIGHT);
         }
 
-        if(Gdx.input.getX() < x + BTN_WIDTH && Gdx.input.getX() > x && Gdx.input.getY() < playBtnY + BTN_HEIGHT && Gdx.input.getY() > playBtnY){
-            game.batch.draw(playBtnHover, x, y, BTN_WIDTH, BTN_HEIGHT);
+        if(Gdx.input.getX() < x + BTN_WIDTH && Gdx.input.getX() > x && Gdx.input.getY() < y - 100 + BTN_HEIGHT && Gdx.input.getY() > y - 100){
+            game.batch.draw(playBtnHover, x, y - 100, BTN_WIDTH, BTN_HEIGHT);
             if(Gdx.input.isTouched()) {
                 this.dispose();
                 game.setScreen(new Level1(game));
             }
         }
         else {
-            game.batch.draw(playBtn,x, y, BTN_WIDTH, BTN_HEIGHT);
+            game.batch.draw(playBtn, x, y - 100, BTN_WIDTH, BTN_HEIGHT);
         }
 
         //game.font.draw(game.batch, "GAME", Gdx.graphics.getWidth() * .25f, Gdx.graphics.getHeight() * .75f);

d6be6adecbf1081831485c89be09bfde8a313d23
guillermo-ggs <ggsparacino@gmail.com>
2020-10-10 23:18:44 -0400

    added expanded map

--- android/assets/textures.tsx
+++ android/assets/textures.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="textures" tilewidth="32" tileheight="32" tilecount="8" columns="8">
- <image source="textures.png" width="256" height="32"/>
+<tileset version="1.4" tiledversion="1.4.2" name="textures" tilewidth="32" tileheight="32" tilecount="16" columns="16">
+ <image source="textures.png" width="512" height="32"/>
 </tileset>
